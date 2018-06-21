4407adf68701deb00844a0137accee28ad6d960b
lenovo <lenovo@lenovo-PC>
2018-06-21 18:12:03 +0800

    项目初步设计

--- weixin-feign/src/main/java/com/jia/weixin/feign/loginpage/ILoginPage.java
+++ weixin-feign/src/main/java/com/jia/weixin/feign/loginpage/ILoginPage.java
@@ -1,5 +1,49 @@
 package com.jia.weixin.feign.loginpage;
 
-public interface ILoginPage {
+import java.util.List;
 
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+import com.weixin.util.Rt;
+import com.weixin.vo.Person;
+import com.weixin.vo.StandingBook;
+
+public interface ILoginPage {
+	/**
+	 * 登录接口
+	 * @param person
+	 * @return
+	 */
+	@RequestMapping(value = { "/login" }, method = {RequestMethod.POST})
+	public Rt<List<Person>> login(@RequestBody Person person);
+	
+	/**
+	 * 查询用户 记账记录
+	 * @param userId
+	 * @return
+	 */
+	@RequestMapping(value = { "/info/{userId}" }, method = {RequestMethod.GET})
+	public Rt<List<StandingBook>> getUserList(@PathVariable("userId")  Long userId);
+	
+	
+	/**
+	 * 标记为已还
+	 * @param userId
+	 * @param rowId
+	 * @return
+	 */
+	@RequestMapping(value = { "/upt/{userId}/{rowId}" }, method = {RequestMethod.PUT})
+	public Rt<String> markRepay(@PathVariable("userId")  Long userId ,@PathVariable("rowId") Long rowId);
+	
+	
+	/**
+	 * 保存一条记账记录
+	 * @param book
+	 * @return
+	 */
+	@RequestMapping(value = { "/save" }, method = {RequestMethod.POST})
+	public Rt<String> saveBook(@RequestBody StandingBook book);
 }
--- weixin-pojo/src/main/java/com/weixin/vo/Person.java
+++ weixin-pojo/src/main/java/com/weixin/vo/Person.java
@@ -1,6 +1,7 @@
 package com.weixin.vo;
 
 public class Person {
+	private Long userId;
 	private Integer age;
 	private String name;
 	public Integer getAge() {
@@ -15,38 +16,11 @@ public class Person {
 	public void setName(String name) {
 		this.name = name;
 	}
-	@Override
-	public String toString() {
-		return "Person [age=" + age + ", name=" + name + "]";
+	public Long getUserId() {
+		return userId;
 	}
-	@Override
-	public int hashCode() {
-		final int prime = 31;
-		int result = 1;
-		result = prime * result + ((age == null) ? 0 : age.hashCode());
-		result = prime * result + ((name == null) ? 0 : name.hashCode());
-		return result;
-	}
-	@Override
-	public boolean equals(Object obj) {
-		if (this == obj)
-			return true;
-		if (obj == null)
-			return false;
-		if (getClass() != obj.getClass())
-			return false;
-		Person other = (Person) obj;
-		if (age == null) {
-			if (other.age != null)
-				return false;
-		} else if (!age.equals(other.age))
-			return false;
-		if (name == null) {
-			if (other.name != null)
-				return false;
-		} else if (!name.equals(other.name))
-			return false;
-		return true;
+	public void setUserId(Long userId) {
+		this.userId = userId;
 	}
 	
 	
