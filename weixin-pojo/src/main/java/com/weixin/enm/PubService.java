09f70f0744f9cebd7d020bed4440070857c76b1d
lenovo <lenovo@lenovo-PC>
2018-06-27 16:48:03 +0800

    服务名枚举类

---
+++ weixin-pojo/src/main/java/com/weixin/enm/PubService.java
@@ -0,0 +1,17 @@
+package com.weixin.enm;
+/**
+ * 服务名
+ * @author lenovo
+ *
+ */
+public class PubService {
+	/**
+	 * Service 服务名
+	 */
+	public static final String NOTE_SERVICE ="NOTE-SERVICE";
+	/**
+	 * api 服务名
+	 */
+	public static final String NOTE_API ="NOTE-API";
+	
+}
