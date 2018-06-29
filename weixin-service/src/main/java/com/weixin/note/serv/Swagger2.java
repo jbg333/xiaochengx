a83599dc765333928d6ccc8e8ae0054322f2aca4
lenovo <lenovo@lenovo-PC>
2018-06-29 23:50:37 +0800

    工具类

---
+++ weixin-service/src/main/java/com/weixin/note/serv/Swagger2.java
@@ -0,0 +1,41 @@
+package com.weixin.note.serv;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import springfox.documentation.builders.ApiInfoBuilder;
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.service.ApiInfo;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+/**
+ * @author chenyi
+ * @date 2017/12/28
+ */
+@Configuration
+@EnableSwagger2
+public class Swagger2 {
+
+    @Bean
+    public Docket createRestApi() {
+        return new Docket(DocumentationType.SWAGGER_2)
+                .apiInfo(apiInfo())
+                .select()
+                .apis(RequestHandlerSelectors.basePackage("com.weixin.note.serv"))
+                .paths(PathSelectors.any())
+                .build();
+    }
+
+    private ApiInfo apiInfo() {
+        return new ApiInfoBuilder()
+                .title("RESTful APIs")
+                .description("service管理项目")
+                .termsOfServiceUrl("http://admin.gonghuikeji.com/")
+                .version("1.0")
+                .build();
+    }
+
+}
