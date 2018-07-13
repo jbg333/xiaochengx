4fd0710e5f750867f9ac711dd46abb5c2b5b5825
lenovo <lenovo@lenovo-PC>
2018-07-13 19:14:56 +0800

    最新

---
+++ weixin-api/src/main/java/com/weixin/note/api/WebMvcConfig.java
@@ -0,0 +1,13 @@
+package com.weixin.note.api;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurationSupport;
+
+@Configuration
+public class WebMvcConfig extends  WebMvcConfigurationSupport{
+	@Override
+    protected void addInterceptors(InterceptorRegistry registry) {
+        super.addInterceptors(registry);
+    }
+}
